6520359913e037fa7761257d0c5d7b5edb1e8764
Herbert Saladar <hrbrtsldr@gmail.com>
2022-05-12 01:02:55 +0800

    Redux integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import TodoList from "./todos/TodoList";
-function App() {
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import styled from 'styled-components';
+import TodoList from './todos/TodoList';
 
-  return (
-    <>
-      <TodoList />
-    </>
-  );
-}
+const AppContainer = styled.div`
+    margin: 1rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #222222;
+    width: 100vw;
+    height: 100vh;
+`;
 
-export default App;
+const App = () => (
+    <AppContainer>
+        <TodoList />
+    </AppContainer>
+);
+
+export default hot(module)(App);
--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,26 +1,69 @@
-import React from "react";
-import { useState } from "react";
-import { connect } from "react-redux";
-import { createTodo } from "./actions";
-const NewTodoForm = ({ todos, onCreatePressed }) =>{
-    const [inputValue, setInputValue ] = useState('')
-    return (
-        <div className="new-todo-form">
-            <input type="text" value={inputValue} />
-            <button className="new-todo-button"  onClick={()=> {
-                const isDuplicatedText = todos.some( todo => todo.text ===  inputValue )
-                if( !isDuplicatedText){
-                    onCreatePressed(inputValue)
-                    setInputValue('')
-                }}}> Create Todo </button>
-        </div>
-        )
-    }
-const mapStateToProps =  state => ({
-    todos: state.todos, 
-})
-const mapDispatchToProps = dispatch => ({
-    onCreatePressed : text => dispatch( createTodo( text ))
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { getTodos } from './selectors';
+import { addTodoRequest } from './thunks';
 
-})
-export default connect(mapStateToProps, mapDispatchToProps )(NewTodoForm)
+const FormContainer = styled.div`
+    border-radius: 8px;
+    padding: 16px;
+    text-align: center;
+    box-shadow: 0 4px 8px grey;
+`;
+
+const NewTodoInput = styled.input`
+    font-size: 16px;
+    padding: 8px;
+    border: none;
+    border-bottom: 2px solid #ddd;
+    border-radius: 8px;
+    width: 70%;
+    outline: none;
+`;
+
+const NewTodoButton = styled.button`
+    font-size: 16px;
+    padding: 8px;
+    border: none;
+    border-radius: 8px;
+    outline: none;
+    cursor: pointer;
+    margin-left: 8px;
+    width: 20%;
+    background-color: #22ee22;
+`;
+
+const NewTodoForm = ({ todos, onCreatePressed }) => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <FormContainer>
+            <NewTodoInput
+                type="text"
+                placeholder="Type your new todo here"
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)} />
+            <NewTodoButton
+                onClick={() => {
+                    const isDuplicateText =
+                        todos.some(todo => todo.text === inputValue);
+                    if (!isDuplicateText) {
+                        onCreatePressed(inputValue);
+                        setInputValue('');
+                    }
+                }}>
+                Create Todo
+            </NewTodoButton>
+        </FormContainer>
+    );
+};
+
+const mapStateToProps = state => ({
+    todos: getTodos(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(addTodoRequest(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
